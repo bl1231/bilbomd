99009a045e4ac616e334488f6e3e95d2ecd15f69
Scott Classen <sclassen@lbl.gov>
2025-09-21 20:16:02 -0700

    add mailer test

--- apps/worker/test/setup.ts
+++ apps/worker/test/setup.ts
@@ -1,3 +1,6 @@
+// Set log directory and mailer template path for all tests before anything else
+process.env.BILBOMD_LOGS = process.env.BILBOMD_LOGS || '/tmp/bilbomd-logs-test'
+process.env.BILBOMD_MAILER_TEMPLATES || __dirname + '../src/templates/mailer'
 import { vi } from 'vitest'
 
 vi.mock('../src/helpers/loggers', () => ({
