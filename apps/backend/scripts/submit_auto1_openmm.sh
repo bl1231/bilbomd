b64041cc6db214664d36bc4c7d867fc643971989
Scott Classen <sclassen@lbl.gov>
2025-09-22 17:40:53 -0700

    fix typo in test script

--- apps/backend/scripts/submit_auto1_openmm.sh
+++ apps/backend/scripts/submit_auto1_openmm.sh
@@ -27,7 +27,7 @@ HTTP_STATUS=$(curl -# -o "$RESPONSE_FILE" -w "%{http_code}" \
   -H "Accept: application/json" \
   -F "bilbomd_mode=auto" \
   -F "md_engine=OpenMM" \
-  -F "title=${TITLE_DATE}-api-test-auto1-${TITLE_SUFFIX}" \
+  -F "title=${TITLE_DATE}-auto1-openmm-${TITLE_SUFFIX}" \
   -F "pdb_file=@${PDB_FILE}" \
   -F "dat_file=@${DAT_FILE}" \
   -F "pae_file=@${PAE_FILE}" )
