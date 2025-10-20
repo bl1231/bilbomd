b45286dac69858d3d1c0fd5d1541e0d82d6d924d
Scott Classen <sclassen@lbl.gov>
2025-10-20 11:22:20 -0700

    fix function call runGenerateMovies

--- apps/worker/src/services/pipelines/dcd-to-mp4.ts
+++ apps/worker/src/services/pipelines/dcd-to-mp4.ts
@@ -6,7 +6,7 @@ import { runGenerateMovies } from '../functions/movie-worker.js'
 const renderMovieJob = async (MQjob: BullMQJob) => {
   logger.info(`Processing DCD to MP4 Job: ${MQjob.data.uuid}`)
   try {
-    await runGenerateMovies(MQjob, MQjob.data)
+    await runGenerateMovies(MQjob)
   } catch (error) {
     logger.error(`Failed to process DCD to MP4 Job: ${MQjob.data.uuid}`)
     throw error
