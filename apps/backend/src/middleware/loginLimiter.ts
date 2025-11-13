185c7949a95cb90da9e9f312019e0038d9cd0781
Scott Classen <sclassen@lbl.gov>
2025-11-13 11:45:34 -0800

    linting lower max login attmpts to 3

--- apps/backend/src/middleware/loginLimiter.ts
+++ apps/backend/src/middleware/loginLimiter.ts
@@ -4,15 +4,20 @@ import { logger } from './loggers.js'
 
 const loginLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 5, // Limit each IP to 5 login requests per `window` per minute
+  max: 3, // Limit each IP to 3 login requests per `window` per minute
   message: {
     message:
       'Too many login attempts from this IP, please try again after a 60 second pause'
   },
-  handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
+  handler: (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    options: Options
+  ) => {
     const clientIp = (req.ip ?? '').includes('::ffff:')
       ? (req.ip ?? '').split('::ffff:')[1]
-      : req.ip ?? ''
+      : (req.ip ?? '')
     logger.error(
       `Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}\t${clientIp}`,
       'errLog.log'
