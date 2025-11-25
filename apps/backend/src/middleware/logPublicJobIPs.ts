be278073b053d563f6da5f4d59791007530eaf01
Scott Classen <sclassen@lbl.gov>
2025-11-25 14:45:20 -0800

    conditional log messages for public job route

--- apps/backend/src/middleware/logPublicJobIPs.ts
+++ apps/backend/src/middleware/logPublicJobIPs.ts
@@ -2,11 +2,18 @@ import { Request, Response, NextFunction } from 'express'
 import { logger } from './loggers.js'
 
 const logPublicJobIPs = (req: Request, res: Response, next: NextFunction) => {
-  logger.info(
-    `Public job request - Client IP (req.ip): ${req.ip}, CF-Connecting-IP: ${req.headers['cf-connecting-ip']}, X-Forwarded-For: ${req.headers['x-forwarded-for']}, X-Real-IP: ${req.headers['x-real-ip']}, Forwarded: ${req.headers['forwarded']}, All IPs (req.ips): ${req.ips?.join(', ')}`
-  )
-  logger.info(`all headers: ${JSON.stringify(req.headers)}`)
+  const deployEnv = process.env.BILBOMD_FQDN
+  const nerscSpinRegex = /^bilbomd-nersc(-dev)?\.bl1231\.als\.lbl\.gov$/
+
+  if (deployEnv && nerscSpinRegex.test(deployEnv)) {
+    logger.info(
+      `NERSC/SPIN Public Job Route - Client IP: ${req.ip}, X-Forwarded-For: ${req.headers['x-forwarded-for']}, X-Real-IP: ${req.headers['x-real-ip']}, All IPs: ${req.ips?.join(', ')}`
+    )
+  } else {
+    logger.info(
+      `Public Job Route - Client IP: ${req.ip}, CF-Connecting-IP: ${req.headers['cf-connecting-ip']}, X-Forwarded-For: ${req.headers['x-forwarded-for']}, X-Real-IP: ${req.headers['x-real-ip']}, All IPs: ${req.ips?.join(', ')}`
+    )
+  }
   next()
 }
-
 export default logPublicJobIPs
