d69db5b253383876064577b8d867772f4aeda3ce
Scott Classen <sclassen@lbl.gov>
2025-12-09 14:34:53 -0800

    add charmm_md_pdb_report_interval

--- apps/backend/src/controllers/jobs/utils/charmmParams.ts
+++ apps/backend/src/controllers/jobs/utils/charmmParams.ts
@@ -3,13 +3,15 @@ import { Request } from 'express'
 import { calculateRgyrRange } from './calculateRgyrRange.js'
 
 const buildCHARMMParameters = (reqBody: Request['body']): ICHARMMParameters => {
-  const { charmm_md_nsteps, rg_min, rg_max } = reqBody
+  const { charmm_md_nsteps, charmm_md_pdb_report_interval, rg_min, rg_max } =
+    reqBody
   return {
     minimize: {},
     heating: {},
     md: {
       nsteps: parseInt(charmm_md_nsteps) || 300000,
-      rgyr: calculateRgyrRange(rg_min, rg_max)
+      pdb_report_interval: parseInt(charmm_md_pdb_report_interval) || 500,
+      rgyr: calculateRgyrRange(parseInt(rg_min), parseInt(rg_max))
     }
   }
 }
