c241be6a0688768e54ca2178b0d632dfe76b629b
Scott Classen <sclassen@lbl.gov>
2025-12-11 09:05:22 -0800

    adjust teh OpenMM and CHARMM DTO interface spec

--- packages/bilbomd-types/src/jobs/charmm.ts
+++ packages/bilbomd-types/src/jobs/charmm.ts
@@ -3,7 +3,9 @@ export interface CHARMMMinimizeParamsDTO {}
 export interface CHARMMHeatingParamsDTO {}
 
 export interface CHARMMMDParamsDTO {
+  nsteps: number
   rgyr: number[]
+  pdb_report_interval: number
 }
 
 export interface CHARMMParametersDTO {
